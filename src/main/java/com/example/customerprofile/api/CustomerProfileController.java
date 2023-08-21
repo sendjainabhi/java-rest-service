c6e8fb589605d711d1acc3cf1045eae00e3c8e91
Mae Large <mlarge@vmware.com>
2023-08-21 15:15:18 -0700

    nudge

--- src/main/java/com/example/customerprofile/api/CustomerProfileController.java
+++ src/main/java/com/example/customerprofile/api/CustomerProfileController.java
@@ -123,7 +123,7 @@ public class CustomerProfileController {
     @ApiResponses({
             @ApiResponse(
                     responseCode = "200",
-                    description = "Customer profiles retrieved successfully, yeet!"
+                    description = "Customer profiles retrieved successfully, nudge!"
             )
     })
     @Transactional(readOnly = true)
